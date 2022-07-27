f24417ff3433a3e2855b1f581a9f2777a4262bb8
Hope <hope_chambers@comcast.com>
2022-07-27 12:54:22 -0400

    first add back to save data

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Write your JavaScript code here!
- //todo checking that it is saving
+window.addEventListener("load", function(){
+
+   let statusCheck = document.getElementById("launchStatusCheck");
+   statusCheck.style.visability="hidden"
+   let form = document.getElementById("launchForm");
+   
+   let pilotStatus= document.getElementById("pilotStatus");
+   let copilotStatus= document.getElementById("copilotStatus");
+   let fuelStatus= document.getElementById("fuelStatus")
+   let cargotStatus= document.getElementById("cargoStatus")
+   
+   form.addEventListener("submit", function(event){
+      event.preventdefault();
+
+   let pilotInput=document.querySelector("input[name=pilotName)");   
+   let copilotInput= document.querySelector("input[name=copilotName]");
+   let fuelLevelInput= document.querySelector("input[name=fuelLevel]");
+   let cargoMassInput= document.querySelector("input[name=cargoMadd]"); 
+   });
+});
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
