bf3ff1f7a3223cd4208d88604262cc3388306539
Hope <hope_chambers@comcast.com>
2022-07-29 15:38:44 -0400

    the final final changes

--- script.js
+++ script.js
@@ -37,11 +37,11 @@ window.addEventListener("load", function(){
       let cargoMassInput= document.querySelector("input[name=cargoMass]");
       
 
-   if(pilotInput.value==="" || copilotInput.value==="" ||fuelLevelInput.value===""|| cargoMassInput.value===""){
-      alert("Field empty please provide information.");
-   }else if(isNaN(pilotInput.value)=== false || isNaN(copilotInput.value)=== false|| isNaN(fuelLevelInput.value)|| isNaN(cargoMassInput.value)){
-      alert("Please enter correct format.")
-   }else{
+      if(pilotInput.value==="" || copilotInput.value==="" ||fuelLevelInput.value===""|| cargoMassInput.value===""){
+         alert("Field empty please provide information.");
+      }else if(isNaN(pilotInput.value)=== false || isNaN(copilotInput.value)=== false|| isNaN(fuelLevelInput.value)|| isNaN(cargoMassInput.value)){
+         alert("Please enter correct format.")
+      }else{
       itemStatus.style.visibility="visible"
       pilotStatus.innerHTML=`Pilot: ${pilotName} is ready for Launch!`;
       copilotStatus.innerHTML=`Copilot: ${copilotName} is ready Launch!`;
@@ -49,19 +49,19 @@ window.addEventListener("load", function(){
       fuelStatus.innerHTML=`Passed Fuel Check`;
       cargoStatus.innerHTML=`Cargo Mass Check Passed.`;
       launchStatus.style.color="green"
-    }   
-   if(fuelLevelInput.value< 10000){   
+      
+         if(fuelLevelInput.value< 10000){   //only run  if no alters happen 
       launchStatus.innerHTML=`Shuttle not ready for launch`;
       fuelStatus.innerHTML=`Not enough fuel for the journey`;
       launchStatus.style.color="red"
-   }
+      }
    
-   if(cargoMassInput.value >10000){
-         launchStatus.innerHTML=`Shuttle not ready for launch`;
-         cargoStatus.innerHTML=`too much mass for the shuttle to take off.`;
-         launchStatus.style.color="red"  
-
-      };
+         if(cargoMassInput.value >10000){  // only runs if no alters happen
+      launchStatus.innerHTML=`Shuttle not ready for launch`;
+      cargoStatus.innerHTML=`too much mass for the shuttle to take off.`;
+      launchStatus.style.color="red" 
+         };
+       }
+    });
    });
-});
 });
